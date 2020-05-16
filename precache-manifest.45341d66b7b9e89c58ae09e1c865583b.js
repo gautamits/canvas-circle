d34e6b9f545511e7e63806e7fd196f608c126609
amit <gautamamits95@gmail.com>
2020-05-16 18:20:36 +0530

    Updates

---
+++ precache-manifest.45341d66b7b9e89c58ae09e1c865583b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0add34be2f5e0264c14fad86723e0f",
+    "url": "/canvas-circle/index.html"
+  },
+  {
+    "revision": "7731a0a19884eae78d28",
+    "url": "/canvas-circle/static/css/main.114b649d.chunk.css"
+  },
+  {
+    "revision": "e8dfe7fac7d380a6c50c",
+    "url": "/canvas-circle/static/js/2.f4448de4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canvas-circle/static/js/2.f4448de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7731a0a19884eae78d28",
+    "url": "/canvas-circle/static/js/main.4fad6dd1.chunk.js"
+  },
+  {
+    "revision": "368dcb4bef4d1e2dcded",
+    "url": "/canvas-circle/static/js/runtime-main.c6a3fad7.js"
+  }
+]);
